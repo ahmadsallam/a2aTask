0184f2025ea3a01d7bde37a6015137222ba09655
Ahmad Sallam  <ahmad.sallam@dreamtechs.co>
2018-08-16 18:49:48 +0300

    First Commit

---
+++ a2aTask/ViewControllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  a2aTask
+//
+//  Created by Ahmad Sallam  on 8/16/18.
+//  Copyright © 2018 Ahmad Sallam . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
